7a18b2c1f9c5ba12fe71e7b80f21195a149245bd
Patrik Nilsson <pn222en@student.lnu.se>
2016-05-27 00:28:58 +0200

    completed module1

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -43,7 +43,7 @@ public class HelloWorld extends PApplet
 
 		// This sets the background color for the Applet.  
 		// Play around with these numbers and see what happens!
-		this.background(200, 200, 200);
+		this.background(100, 500, 500);
 		
 		// Select a map provider
 		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
@@ -76,6 +76,14 @@ public class HelloWorld extends PApplet
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
+		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+
+		// The next line zooms in and centers the map at 
+	    // 32.9 (latitude) and -117.2 (longitude)
+	    map2.zoomAndPanTo(zoomLevel, new Location(57.6939660,11.9256250));
+		
+		// This line makes the map interactive
+		MapUtils.createDefaultEventDispatcher(this, map2);
 
 	}
 
@@ -84,6 +92,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
